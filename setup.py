541c50704c4d09df239d1f49c35c8292eeee3d30
Daniel Welch <dwelch2102@gmail.com>
2015-06-04 23:46:01 -0400

    setup.py added

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='Xmlstats-py',
+    version='0.0.1',
+    description='Python client for xmlstats API',
+    url='https://github.com/dwelch2101/Xmlstats-py',
+    author='Daniel Welch',
+    author_email='dwelch2012@gmail.com',
+    packages=['xmlstats'],
+    scripts=[],
+    long_description='',
+    license='MIT',
+    install_requires=['requests'],
+)
